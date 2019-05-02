3ef7d870fdee5af7816c4025ac0b21464af77ecc
Alejandro Cortes <acortes@Alejandros-MacBook-Pro.local>
2019-05-01 22:51:02 -0400

    Basic implementation of search button

--- tweetful/src/components/SearchForm.js
+++ tweetful/src/components/SearchForm.js
@@ -2,19 +2,38 @@ import React, {useState} from 'react';
 
 const SearchForm = ()=>{
     const [keyword, setKeyword] = useState("");
-
+    const [isOn, setIsOn] = useState(false);
+    
     return(
-        <form>
-            <input value={keyword}
-            onChange={e => setKeyword(e.target.value)} 
-            placeholder="Type a hashtag"
-            type="text"name="firstName" 
-            required>
-            </input>
-
-            <button type="submit">Submit</button>
-    </form>
+        isOn?
+        <div>
+            <p>Looking for {keyword}</p>
+            <button onClick={e =>foo()}>Want to try a different hashtag?</button> 
+        </div>
+        :
+        <div>
+            <form>
+                <input value={keyword}
+                onChange={e => setKeyword(e.target.value)} 
+                placeholder="Type a hashtag"
+                type="text"name="isOn" 
+                required>
+                </input>
+                
+                <button type="submit" onClick={e =>foo()}>Submit</button>
+            </form>                        
+        </div>    
     )
+    function foo(){
+        // Reset keyword hook if there's already one
+        if(isOn){
+            setKeyword("");
+        }
+        setIsOn(!isOn);
+    }
 }
+
+
+
 
 export default SearchForm;
