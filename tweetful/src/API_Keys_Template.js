4d4778b41405545289603c5f578819007e9a0325
Alejandro Cortes <acortes@Alejandros-MacBook-Pro.local>
2019-05-07 14:13:53 -0400

    Init API Key Template

---
+++ tweetful/src/API_Keys_Template.js
@@ -0,0 +1,10 @@
+const API_Keys_Template = {
+    consumerKey: "placeholder1",
+    consumerSecret: "placeholssder2",
+    RFC_ConsumerKey: "plassceholder3",
+    RFC_SecretKey: "placsseholder4",
+    bearerToken: "placeholder5",
+    base64EncodedBearerToken: "placeholder6"
+    }
+    
+    export default API_Keys_Template;
