4cee8d3b32d066ae38d9c48b981f8c6c35201873
Alejandro Cortes <efrain.alejandro.cortes@gmail.com>
2020-05-22 18:54:56 -0400

    Visual fix

--- tweetful/src/components/tweets/VisualTweets.js
+++ tweetful/src/components/tweets/VisualTweets.js
@@ -24,9 +24,9 @@ const VisualTweets = (props)=> {
   
  
 
-  let items = getTweets(0);
-  let items2 = getTweets(4);
-  let items3 = getTweets(8);
+  let items = getTweets(6);
+  let items2 = getTweets(0);
+  let items3 = getTweets(12);
 
   const trail = useTrail(items.length, {
     from: {color: '#000', opacity: 0},
