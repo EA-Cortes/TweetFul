5d39bd3976942e8a8d426fe761e93fb8d929f138
Alejandro Cortes <efrain.alejandro.cortes@gmail.com>
2020-05-24 03:06:57 -0400

    Added hook and fetch to get collection names

--- tweetful/src/components/SearchPage.js
+++ tweetful/src/components/SearchPage.js
@@ -1,9 +1,26 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import SearchForm from './SearchForm';
 import '../style/SearchPage.css';
 
 
 const SearchPage = ()=> {
+    useEffect(() =>{
+        fetchDBs();
+    }, []);
+
+    const [DBs, setDBs] = useState([]);
+
+    const fetchDBs = async () =>{
+        const data = await fetch(
+            '/getDBs'
+        );
+
+        const dbs = await data.json();
+        console.log(dbs);
+    }
+
     return(
         <div className="searchPageWrapper">
             <div className="searchPageHalf">
@@ -15,6 +32,15 @@ const SearchPage = ()=> {
             
         </div>
     );
+
+    function getDBs(){
+        axios.get(`/getDBs`)
+        .then(res => {
+          console.log("response in server");
+        //   console.log(res);
+        //   console.log(res.data);
+        })
+    }
 }
 
 export default SearchPage;
