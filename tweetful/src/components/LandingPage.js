dc3a3ac59661294a1455f027122aceb9a668cd93
Alejandro Cortes <efrain.alejandro.cortes@gmail.com>
2020-05-27 03:23:32 -0400

    Added Landing Page

---
+++ tweetful/src/components/LandingPage.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+import '../style/App.css';
+import LogIn from './LogIn.js';
+
+const LandingPage = ()=> {
+
+    return(
+        <div>
+            <div className="body">
+                <div className="App">
+                    <div className="leftBar">
+                        <h1>SerTweet</h1>
+                        <p> A fresh search of tweets </p>
+                        <LogIn/>
+
+                    </div>
+                    <div className="content">
+                        <h1>{"Ser Tweet demo"}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default LandingPage;
