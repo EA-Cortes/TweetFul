bf004517ccb2773b3af0369e1e6ca4976a841ed7
Alejandro Cortes <efrain.alejandro.cortes@gmail.com>
2020-05-27 01:09:02 -0400

    add LoggedIn state

--- tweetful/src/App.js
+++ tweetful/src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style/App.css';
 import Header from './components/layout/Header.js';
 import Footer from './components/layout/Footer.js';
 import SearchPage from './components/SearchPage'
 import DataAnalytics from './components/DataAnalytics'
+import Account from './components/Account'
 import Tweet from './components/tweets/Tweet';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import TwitterAPI from './TwitterAPI';
+import { useSprings } from 'react-spring';
 
 // import VisualTweets from './components/tweets/VisualTweets.js';
 
 const App = () =>{    
+  
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(()=>{
+    // setLoggedIn(true);
+  });
   return (    
+    loggedIn?
     <div>
     <div className="Body">
-    
-      
-      {/* <Header/> */}
         <div className="App">
         <Router>
           <div className="leftBar">
@@ -26,7 +32,7 @@ const App = () =>{
            <nav>
             <ul className="listStyle">
             <Link to="/">
-              <li>Account</li>
+              <li>Home</li>
             </Link>
             
             <Link to="/search">
@@ -34,12 +40,17 @@ const App = () =>{
             </Link>
             
             <Link to="/viewTweets">
-              <li>Display</li>
+              <li>Display Tweets</li>
             </Link>
 
             <Link to="/dataAnalytics">
               <li>Data analytics</li>
             </Link>
+
+            <Link to="/account">
+              <li>Account</li>
+            </Link>
+            
 
             <Link to="/signOut">
               <li>Sign Out</li>
@@ -54,6 +65,7 @@ const App = () =>{
               <Route path="/search" component={SearchPage} />
               <Route path="/viewTweets" component={Tweet} />
               <Route path="/dataAnalytics" component={DataAnalytics}/>
+              <Route path="/account" component={Account}/>
               <Route exact path="/" component={""} />                        
             </Switch>
            </div>
@@ -63,13 +75,17 @@ const App = () =>{
           
 
           </Router>
-
-         
+          true
         </div>
          
         <Footer/>    
     </div>
     
+    </div>
+    :
+    <div className="Body">
+    <Header/>
+      <h1>{"Not logged in"}</h1>
     </div>
   );
   
